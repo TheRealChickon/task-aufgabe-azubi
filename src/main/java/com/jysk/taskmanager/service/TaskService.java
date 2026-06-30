d782eb9e8680a636e0657ee03028664c59bcb31b
Sami Zerfaoui <121903761+TheRealChickon@users.noreply.github.com>
2026-06-30 09:34:55 +0200

    further devd

--- src/main/java/com/jysk/taskmanager/service/TaskService.java
+++ src/main/java/com/jysk/taskmanager/service/TaskService.java
@@ -4,6 +4,7 @@ package com.jysk.taskmanager.service;
 import com.jysk.taskmanager.model.Task;
 import com.jysk.taskmanager.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -31,18 +32,21 @@ public class TaskService {
         taskRepository.save(task);
     }
 
-    public void deleteTask(Task task) {
-        taskRepository.delete(task);
+    //    public void deleteTask(Task task) {
+    //        taskRepository.delete(task);
+    //    }
+
+    public void deleteTask(Long id){
+        taskRepository.deleteById(id); // DELETE FROM tasks WHERE id = 5;
     }
 
-    public void toggleTask(Task task) {
-        // 1. Den aktuellen Boolean-Wert auslesen und umdrehen (! = Invertierung)
-        // Hinweis: Falls dein Getter in der Entity 'getStatus()' heißt, passe es kurz an.
-        task.setStatus(!task.getStatus());
 
-        // 2. Das veränderte Objekt wieder abspeichern -> JPA führt automatisch ein UPDATE aus
+    // must use task because takrepo doesnt have a toggle
+    public void toggleTask(Long id){
+        Task task = taskRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Task with id " + id + " not found"));
+
+        task.setStatus(!task.getStatus());
         taskRepository.save(task);
     }
-
 
 }
